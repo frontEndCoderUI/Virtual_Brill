18b2a5b91c80a06ae01866670a9aa542856f2a78
unknown <sureshchalmarad@gmail.com>
2024-05-12 18:19:15 +0530

    completed 5 pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,25 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 0) {
-      navbar.classList.add('fixed');
-    } else {
-      navbar.classList.remove('fixed');
-    }
-  });
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
 
-  
+window.addEventListener("DOMContentLoaded", function(){
+  const menuBtnEl = document.getElementById("menu-offcanvas");
+  menuBtnEl.classList.remove("d-show");
+  menuBtnEl.classList.add("d-none");
+})
+
+function openMenu() {
+  const menuBtnEl = document.getElementById("menu-offcanvas");
+  menuBtnEl.classList.add("d-show");
+  menuBtnEl.classList.remove("d-none");
+}
+function closeMenu() {
+  const menuBtnEl = document.getElementById("menu-offcanvas");
+  menuBtnEl.classList.remove("d-show");
+  menuBtnEl.classList.add("d-none");
+}
