48be6b95238f9c4dbb23d0fc630b46087bdf8b41
frontEndCoder <sureshchalmarad@gmail.com>
2024-05-23 23:50:24 +0530

    merged all services page in single file

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
+const VAServiceSectionElement = document.getElementById("va-service-section");
+const ITSolutionSectionElement = document.getElementById("it-soln-section");
+const AdminServiceSectionElement = document.getElementById(
+  "admin-service-section"
+);
+
 window.addEventListener("scroll", function () {
   var navbar = document.querySelector(".navbar");
   if (window.scrollY > 0) {
@@ -7,11 +13,11 @@ window.addEventListener("scroll", function () {
   }
 });
 
-window.addEventListener("DOMContentLoaded", function(){
+window.addEventListener("DOMContentLoaded", function () {
   const menuBtnEl = document.getElementById("menu-offcanvas");
   menuBtnEl.classList.remove("d-show");
   menuBtnEl.classList.add("d-none");
-})
+});
 
 function openMenu() {
   const menuBtnEl = document.getElementById("menu-offcanvas");
@@ -24,15 +30,39 @@ function closeMenu() {
   menuBtnEl.classList.add("d-none");
 }
 
-// Service Page navigation 
-function navToVAssign(){
-  window.location.href = './virtual-assistance.html'
+// Service Page navigation
+function navToVAssign(current) {
+  VAServiceSectionElement.classList.remove("d-none");
+  VAServiceSectionElement.classList.add("d-show");
+  if (current == "it") {
+    ITSolutionSectionElement.classList.remove("d-show");
+    ITSolutionSectionElement.classList.add("d-none");
+  } else {
+    AdminServiceSectionElement.classList.remove("d-show");
+    AdminServiceSectionElement.classList.add("d-none");
+  }
 }
 
-function navToItSoln(){
-  window.location.href = './it-solutions.html'
+function navToItSoln(current) {
+  ITSolutionSectionElement.classList.remove("d-none");
+  ITSolutionSectionElement.classList.add("d-show");
+  if (current == "va") {
+    VAServiceSectionElement.classList.remove("d-show");
+    VAServiceSectionElement.classList.add("d-none");
+  } else {
+    AdminServiceSectionElement.classList.remove("d-show");
+    AdminServiceSectionElement.classList.add("d-none");
+  }
 }
 
-function navToAdminService(){
-  window.location.href = './admin-services.html'
+function navToAdminService(current) {
+  AdminServiceSectionElement.classList.remove("d-none");
+  AdminServiceSectionElement.classList.add("d-show");
+  if (current == "va") {
+    VAServiceSectionElement.classList.remove("d-show");
+    VAServiceSectionElement.classList.add("d-none");
+  } else {
+    ITSolutionSectionElement.classList.remove("d-show");
+    ITSolutionSectionElement.classList.add("d-none");
+  }
 }
